84d9150e7c4f237b31d9d1995b7c448c28a26add
i-am-immortal <127956782+i-am-immortal@users.noreply.github.com>
2023-09-07 13:30:04 +0530

    script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -10,6 +10,9 @@ arr.forEach((button) => {
     } else if (e.target.innerHTML == "AC") {
       string = "";
       input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
     } else {
       string += e.target.innerHTML;
       input.value = string;
